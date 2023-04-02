c79141c01b38c298d769520ecd64f9299168ebfb
Fajar Julyana <90027748+fajarjulyana@users.noreply.github.com>
2023-04-02 14:55:53 +0700

    Create code.ino

---
+++ code.ino
@@ -0,0 +1,30 @@
+int ledPin = 13;
+
+void setup() {
+
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(9600);
+
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+
+    int state = Serial.read();
+
+    if (state == '1') {
+
+      digitalWrite(ledPin, HIGH);
+
+    } else if (state == '0') {
+
+      digitalWrite(ledPin, LOW);
+
+    }
+
+  }
+
+}
+
